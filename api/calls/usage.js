153e46aeca92205cd17b6e6b896565e580c84629
Brandon Loeffler <brandon_l121@yahoo.com>
2023-06-15 01:07:13 -0400

    Updated calls

--- api/calls/usage.js
+++ api/calls/usage.js
@@ -1,2 +1,18 @@
 var Schoox = require('../schoox.js');
 var extend = require('xtend');
+
+//#region GET /usage
+/**
+ * Returns the remaining calls according to your plan.
+ * @callback		complete
+ * @memberof        Schoox
+ * @method          dashboardGetUsers
+ */
+
+ Schoox.prototype.getUsage = function (callback) {
+
+	this._get('/usage', options, function (error, body) {
+		callback(error, body);
+	});
+};
+//#endregion
