a8448befa97f25a92c3eeddea082512684345c18
Brandon <loeffler.1287@gmail.com>
2023-07-08 10:44:02 -0500

    Added _delete

--- api/schoox.js
+++ api/schoox.js
@@ -4,109 +4,127 @@ const fs = require('fs');
 const path = require('path');
 
 class Schoox {
-  #credentials;
+	#credentials;
 
-  constructor(acad_id, api_key, env) {
-    switch (env) {
-      case 'stage':
-        this.baseURL = 'https://staging.schoox.com/api/v1';
-        break;
-      case 'prod':
-        this.baseURL = 'https://api.schoox.com/v1';
-        break;
-      default:
-        this.baseURL = 'https://api.schoox.com/v1';
-    }
+	constructor(acad_id, api_key, env) {
+		switch (env) {
+			case 'stage':
+				this.baseURL = 'https://staging.schoox.com/api/v1';
+				break;
+			case 'prod':
+				this.baseURL = 'https://api.schoox.com/v1';
+				break;
+			default:
+				this.baseURL = 'https://api.schoox.com/v1';
+		}
 
-    this.#credentials = {
-      acadId: acad_id,
-      apikey: api_key,
-    };
+		this.#credentials = {
+			acadId: acad_id,
+			apikey: api_key,
+		};
 
-    // Would like to use this but intellisense can't pick it up.
-    //this._loadCalls();
+		// Would like to use this but intellisense can't pick it up.
+		// Was recommended to use an import instead of a request for this. Can give it a try later but will have to convert everything to ES5.
+		//this._loadCalls();
 
-    this.usage = require('./calls/usage')(this);
-    this.users = require('./calls/users')(this);
-    this.groups = require('./calls/groups')(this);
-    this.exams = require('./calls/exams')(this);
-    this.events = require('./calls/events')(this);
-    this.dashboard = require('./calls/dashboard')(this);
-    this.curriculums = require('./calls/curriculums')(this);
-    this.courses = require('./calls/courses')(this);
-    this.content = require('./calls/content')(this);
-    this.badges = require('./calls/badges')(this);
-  }
+		this.usage = require('./calls/usage')(this);
+		this.users = require('./calls/users')(this);
+		this.groups = require('./calls/groups')(this);
+		this.exams = require('./calls/exams')(this);
+		this.events = require('./calls/events')(this);
+		this.dashboard = require('./calls/dashboard')(this);
+		this.curriculums = require('./calls/curriculums')(this);
+		this.courses = require('./calls/courses')(this);
+		this.content = require('./calls/content')(this);
+		this.badges = require('./calls/badges')(this);
+	}
 
-  // See comment on line #26
-  // _loadCalls() {
-  //   const callsDirectory = path.join(__dirname, 'calls');
-  //   const files = fs.readdirSync(callsDirectory);
+	// See comment on line #26
+	// _loadCalls() {
+	//   const callsDirectory = path.join(__dirname, 'calls');
+	//   const files = fs.readdirSync(callsDirectory);
 
-  //   files.forEach(file => {
-  //     const fileName = path.parse(file).name;
-  //     const funcName = fileName.replace('.js', '');
+	//   files.forEach(file => {
+	//     const fileName = path.parse(file).name;
+	//     const funcName = fileName.replace('.js', '');
 
-  //     this[funcName] = require(`./calls/${fileName}`)(this);
-  //   });
-  // }
+	//     this[funcName] = require(`./calls/${fileName}`)(this);
+	//   });
+	// }
 
-  _get(url, parameters, callback) {
-    const config = {
-      baseURL: this.baseURL,
-      params: extend(parameters, this.#credentials),
-    };
+	_get(url, parameters, callback) {
+		const config = {
+			baseURL: this.baseURL,
+			params: extend(parameters, this.#credentials),
+		};
 
-    console.log('GET:', url);
+		console.log('GET:', url);
 
-    axios
-      .get(url, config)
-      .then(function (response) {
-        if (response.status !== 200) {
-          throw new Error(`Status was not OK. ${response.status}`);
-        }
-        callback(null, response.data || {});
-      })
-      .catch(function (error) {
-        callback(error, {});
-      });
-  }
+		axios
+			.get(url, config)
+			.then(function (response) {
+				if (response.status !== 200) {
+					throw new Error(`Status was not OK. ${response.status}`);
+				}
+				callback(null, response.data || {});
+			})
+			.catch(function (error) {
+				callback(error, {});
+			});
+	}
 
-  _put(url, parameters, requestObject, callback) {
-    const config = {
-      baseURL: this.baseURL,
-      params: extend(parameters, this.#credentials),
-    };
+	_put(url, parameters, requestObject, callback) {
+		const config = {
+			baseURL: this.baseURL,
+			params: extend(parameters, this.#credentials),
+		};
 
-    console.log('PUT:', url);
+		console.log('PUT:', url);
 
-    axios
-      .put(url, requestObject, config)
-      .then(function (response) {
-        callback(null, response.data || {});
-      })
-      .catch(function (error) {
-        callback(error, {});
-      });
-  }
+		axios
+			.put(url, requestObject, config)
+			.then(function (response) {
+				callback(null, response.data || {});
+			})
+			.catch(function (error) {
+				callback(error, {});
+			});
+	}
 
-  _post(url, requestObject, callback) {
-    const config = {
-      baseURL: this.baseURL,
-      params: this.#credentials,
-    };
+	_post(url, requestObject, callback) {
+		const config = {
+			baseURL: this.baseURL,
+			params: this.#credentials,
+		};
 
-    console.log('POST:', url);
+		console.log('POST:', url);
 
-    axios
-      .post(url, requestObject, config)
-      .then(function (response) {
-        callback(null, response || {});
-      })
-      .catch(function (error) {
-        callback(error, {});
-      });
-  }
+		axios
+			.post(url, requestObject, config)
+			.then(function (response) {
+				callback(null, response || {});
+			})
+			.catch(function (error) {
+				callback(error, {});
+			});
+	}
+	_delete(url, parameters, callback) {
+		const config = {
+			baseURL: this.baseURL,
+			params: extend(parameters, this.#credentials),
+		};
+
+		console.log('DELETE:', url);
+
+		axios
+			.delete(url, config)
+			.then(function (response) {
+				callback(null, response || {});
+			})
+			.catch(function (error) {
+				callback(error, {});
+			});
+	}
 }
 
 module.exports = Schoox;
